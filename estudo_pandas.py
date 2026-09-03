1b1a59c65ec4e72f7a1592c2e4a9ccbd26a6c4c7
Vitor Duan <vitorduandev@gmail.com>
2026-09-03 13:36:32 -0300

    03/09/26
    Added various data analysis print statements for temperature and ammonia metrirealizaçao de mais exercicooscs.

--- estudo_pandas.py
+++ estudo_pandas.py
@@ -156,7 +156,7 @@ print(df[(df["temperatura_ar"] > 30) & (df["amonia"] > 10)])
 print()
 
 print("todos registros que tiveram a temperatura_ar menor que 25 E amonia menor que 5")
-print(df[(df["temperatura_ar"] > 25) & (df["amonia"] < 5)])
+print(df[(df["temperatura_ar"] < 25) & (df["amonia"] < 5)])
 
 print()
 
@@ -177,5 +177,86 @@ print()
 
 print("Descubra a maior concentração de amonia somente quando: temperatura_ar for maior que 30")
 print(f"a maior concentracao de amonia nesse intervalo foi de {df[df["temperatura_ar"] > 30] ["temperatura_ar"].max():.2f}")
+
+
+print("------TREINAMENTO DIA 03/09/26")
+print()
+
+print("Mostre quanros registros existem no DataSet:")
+print(f"Existem {df.shape[0]} registros no dataset")
+
+print()
+
+print("Calcule a média da coluna temperatura_ar:")
+print(f"A media das temperaturas da coluna temperatura_ar é de {df["temperatura_ar"].mean():.2f}")
+
+print()
+
+print("Calcule a média da coluna amonia:")
+print(f"A media na coluna amonia foi de {df['amonia'].mean()}")
+
+print()
+
+print("Descubra a maior temperatura registrada:")
+print(f"A maior temperatura registrada foi de {df['temperatura_ar'].max()}")
+
+print()
+
+print("Descubra a menor temperatura registrada")
+print(f"A menor temperatura registrada foi de {df['temperatura_ar'].min()}")
+
+print()
+
+print("Descubra a menor concentração de amônia registrada")
+print(f"A menor concentração de amonia utilizada foi de {df['amonia'].min()}")
+
+print("Mostre somente as colunas: timestamp, temperatura_ar, umidade_relativa")
+print(df[["timestamp", "temperatura_ar", "umidade_relativa"]])
+
+print()
+
+print("Mostre somente: amonia, temperatura_globo_negro, itgu")
+print(df[["amonia", "temperatura_globo_negro", "itgu"]])
+
+print()
+
+print("Descubra o tipo de dado (dtype) de cada coluna.")
+print(df.dtypes)
+
+print("Descubra quantos valores únicos existem na coluna timestamp")
+print(f"existem {df["timestamp"].nunique()} valores unicos na coluna")
+
+print()
+
+print("Descubra se existem valores nulos em alguma das 7 colunas.")
+print(df.isnull().sum())
+
+print("Calcule a média de: temperatura_ar, umidade_relativa, amonia, temperatura_globo_negro, ponto_orvalho, itgu")
+print(df[["temperatura_ar", "umidade_relativa", "amonia", "temperatura_globo_negro", "ponto_orvalho", "itgu"]].mean())
+
+print()
+
+print("Descubra a maior umidade relativa:")
+print(f"A maior umidade relativa foi de {df["umidade_relativa"].max()}")
+
+print()
+
+print("Descubra a menor umidade relativa.")
+print(f"A menor umidade relativa foi de {df["umidade_relativa"].min()}")
+
+print()
+
+print("Descubra a maior temperatura de globo negro.")
+print(f"A maior temperatura do globo negro foi de {df["temperatura_globo_negro"].max()}")
+
+print("Descubra o maior valor de ITGU.")
+print(f"o maior valor do itgu foi de {df["itgu"].max()}")
+
+print("Descubra o menor valor de ITGU.")
+print(f"o menor valor do itgu foi de {df['itgu'].min()}")
+
+
+
+
 
 
